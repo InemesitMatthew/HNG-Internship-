af632b7afd6fe886e4b364ff3b3087cc5928449a
Senmid <inemesit.e.matthew@gmail.com>
2025-02-03 21:00:05 +0100

    Implement home page and dark theme for the Xorify app

---
+++ Task1-DataEncryptionDecryption/xorify/lib/themes/theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+// Defines the dark theme for the app
+ThemeData darkMode = ThemeData(
+  colorScheme: ColorScheme.dark(
+    surface: const Color.fromARGB(255, 20, 20, 20),
+    primary: const Color.fromARGB(255, 122, 122, 122),
+    secondary: const Color.fromARGB(255, 30, 30, 30),
+    tertiary: const Color.fromARGB(255, 47, 47, 47),
+    inversePrimary: Colors.grey.shade300,
+    // Add a consistent button color
+    primaryContainer: const Color(0xFF0D6EFD), // Slightly darker blue for dark mode
+  ),
+);
