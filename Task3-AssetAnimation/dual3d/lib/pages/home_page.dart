50b8e6cedaa1593c7166e6eaca273f7718fb4283
Senmid <inemesit.e.matthew@gmail.com>
2025-02-18 19:45:50 +0100

    Remove unused 3D model assets and update pubspec.yaml to include new male and female models; enhance home page layout with ModelViewer integration for animations.

--- Task3-AssetAnimation/dual3d/lib/pages/home_page.dart
+++ Task3-AssetAnimation/dual3d/lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:model_viewer_plus/model_viewer_plus.dart';
 import '../components/theme_toggle.dart';
 
 class HomePage extends StatefulWidget {
@@ -16,6 +17,46 @@ class _HomePageState extends State<HomePage> {
         title: const Text('Dual 3D'),
         actions: const [
           ThemeToggle(),
+        ],
+      ),
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: const Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // Male Animation Button
+
+                // Female Animation Button
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Expanded(
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: ModelViewer(
+                      src: "lib/images/male.glb",
+                      autoRotate: true,
+                    ),
+                  ),
+                  Expanded(
+                    child: ModelViewer(
+                      src: "lib/images/female.glb",
+                      autoRotate: true,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
